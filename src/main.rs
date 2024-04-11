67d021e58161f65c28bfd2fe6c8f9601cb84eaed
Jan <13823649+JanMaier97@users.noreply.github.com>
2024-04-11 12:58:26 +0200

    Add clap and first cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use anki_utils::{field_validation, MyResult};
 use anki_utils::field_validation::ValidationConfig;
 
+use clap::Parser;
+
 use std::fs::File;
 use std::io::BufReader;
 
+#[derive(Parser)]
+#[command(name = "Anki Field Validator")]
+#[command(version, about)]
+#[command(long_about = "Anki Field Validator is a command line tool to validate notes in anki.\n This tool requires the AnkiConnect plugin to be installed in order to interact with Anki.")]
+struct CliArgs {
+    config_file: String,
+}
+
 fn main() -> MyResult<()> {
-    let f = File::open("./config/test-validation.json")?;
+    let cli = CliArgs::parse();
+
+    let f = File::open(&cli.config_file)?;
     let reader = BufReader::new(f);
     let config: ValidationConfig = serde_json::from_reader(reader)?;
+
     let result = field_validation::execute(&config)?;
 
     dbg!(result);
